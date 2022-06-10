dc199c3d0e87b96032867b16fe7009662a5be1bf
Esteban <esteban@fwd.dev>
2022-06-09 22:56:16 -0400

    rover

--- n2.sh
+++ n2.sh
@@ -235,7 +235,8 @@ EOF
 
 	echo ""
 
-	    echo $(curl -g -d '{ "action": "telemetry" }' '[::1]:7076' | jq)
+  echo $(curl -g -d '{ "action": "telemetry" }' '[::1]:7076' | jq)
+  
 	if curl -s --fail -X GET '[::1]:7076'; then
 	else
 	    echo "${RED}Error${NC}: No local Node found. Use 'n2 local install'"

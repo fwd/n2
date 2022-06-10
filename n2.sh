e3a9d2449ec8f82dffb81af06ef5426e8d878442
Esteban <esteban@fwd.dev>
2022-06-09 23:00:18 -0400

    rover

--- n2.sh
+++ n2.sh
@@ -235,13 +235,13 @@ EOF
 
 	echo ""
 
-  echo $(curl -g -d '{ "action": "telemetry" }' '[::1]:7076' | jq)
 
-	# if curl -s --fail -X GET '[::1]:7076'; then
-	# 	echo ""
-	# else
-	#    echo "${RED}Error${NC}: No local Node found. Use 'n2 local install'"
-	# fi;
+	if curl -s --fail -d '{ "action": "telemetry" }' '[::1]:7076'; then
+  	echo $(curl -g -d '{ "action": "telemetry" }' '[::1]:7076' | jq)
+		# echo ""
+	else
+	   echo "${RED}Error${NC}: No local Node found. Use 'n2 local install'"
+	fi;
 
 	# echo "================================="
 	# echo "       UNDER CONSTRUCTION        "

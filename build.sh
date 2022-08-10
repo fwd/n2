09e711ed4914a7be7d547e402cbcce13a53288a7
Esteban <esteban@fwd.dev>
2022-08-09 22:09:49 -0400

    stable

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-cat ./chunks/*.sh > ./n2.sh
+cat ./source/*.sh > ./n2.sh
 file_size_kb=`du -k "./n2.sh" | cut -f1`
 chmod +x ./n2.sh
-echo "$(ls -1q ./chunks/* | wc -l | xargs) files,combined into one. ($file_size_kb kb)"
+echo "$(ls -1q ./source/* | wc -l | xargs) files in source. Combined: ($file_size_kb kb)"
 
